83fed55899ed32d9b350126e1a9af35d253eb044
Krosver <kirill.kutyrev@gmail.com>
2022-03-01 01:46:39 +0300

    create MyQueue

---
+++ MyQueue/Queue/WinFormsApp1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public MyQueue<int> new_queue;
+        public int[] mas;
+        int size;
+        private void bt_create_Click(object sender, EventArgs e)
+        {
+            size = Convert.ToInt32(tb_create.Text);
+            new_queue = new MyQueue<int>(size);
+            tb_create.Text = "";
+            queue.Items.Clear();
+        }
+
+        private void bt_enqueue_Click(object sender, EventArgs e)
+        {
+            if (queue.Items.Count < size)
+            {
+                new_queue.Enqueue(Convert.ToInt32(tb_enqueue.Text));
+                tb_enqueue.Text = "";
+                this.print_queue();
+            }
+            else
+            {
+                MessageBox.Show("Массив переполнен");
+            }
+
+        }
+
+        private void bt_dequeue_Click(object sender, EventArgs e)
+        {
+            if (queue.Items.Count == 0)
+            {
+                MessageBox.Show("Массив пуст");
+            }
+            else
+            {
+                tb_dequeue.Text = Convert.ToString(new_queue.Dequeue());
+                this.print_queue();
+            }
+
+        }
+
+        private void bt_peek_Click(object sender, EventArgs e)
+        {
+            tb_peek.Text = Convert.ToString(new_queue.Peek());
+        }
+        private void print_queue()
+        {
+            mas = new_queue.ToArray();
+            queue.Items.Clear();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                queue.Items.Insert(i, mas[i]);
+            }
+        }
+    }
+}
---
+++ MyQueue/Queue/WinFormsApp1/MyQueue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp1
+{
+    public class MyQueue<T>
+    {
+        private int currentIn;
+        private int currentOut;
+        public T[] arr;
+        public MyQueue(int size)
+        {
+            arr = new T[size];
+            currentIn = 0;
+            currentOut = 0;
+        }
+        public void Enqueue( T val)
+        {
+
+            if (currentIn >= arr.Length)
+            {
+                currentIn = 0;
+                arr[currentIn] = val;
+            }
+            else
+            {
+                arr[currentIn] = val;
+                currentIn++;
+            }
+        }
+        public T Dequeue()
+        {
+            T ret = arr[currentOut];
+            if (currentOut <= arr.Length)
+                currentOut++;
+            else
+                currentOut = 0;
+            return ret;
+        }
+        public T Peek()
+        {
+            return (arr[currentOut]);
+        }
+        public T[] ToArray()
+        {
+            int current;
+            int current_mas_out = currentOut;
+            if (currentIn > currentOut)
+                current = currentIn - currentOut;
+            else
+                current = 0;
+            T[] mas = new T[current];
+            for (int i = 0; i <= current - 1; i++)
+            {
+                mas[i] = arr[current_mas_out];
+                if (current_mas_out == arr.Length - 1)
+                    current_mas_out = 0;
+                else
+                    current_mas_out++;
+            }
+            return mas;
+        }
+    }
+}
