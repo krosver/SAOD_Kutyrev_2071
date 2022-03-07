1f70cde224eb849e20b0d37eff047b89fe9ec842
Krosver <kirill.kutyrev@gmail.com>
2022-03-07 21:46:29 +0300

    Creat opz

---
+++ OPZ/OPZ/MyOPZ.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPZ
+{
+    public class MyOPZ
+    {
+        private int current;
+        private int priority;
+        private string[] arr;
+        private string[] opz_arr;
+
+        public MyOPZ(string str)
+        {
+            arr = new string[str.Length];
+
+        }
+
+        public string[] ToOPZ()
+        {
+            opz_arr = new string[current];
+            for (int i = 0; i < current; i++)
+            {
+                if (arr[i] == "(")
+                    Parenthesis();
+
+                if (arr[i] == "+" || arr[i] == "-" || arr[i] == "/" || arr[i] == "*" || arr[i] == "^")
+                {
+                    string opperant = arr[i];
+                    if (opz_arr[i + 1] != "(")
+                    {
+                        opz_arr[i] = arr[i + 1];
+                        opz_arr[i + 1] = opperant;
+                        i++;
+                    }
+                    else
+                    {
+                        while (opz_arr[i] != ")")
+                        {
+
+                        }
+                    }
+                }
+                else
+                {
+                    opz_arr[i] = arr[i];
+                }
+            }
+            return opz_arr;
+        }
+        public void Enqueue(string val)
+        {
+            arr[current] = val;
+            current++;
+        }
+        public void Parenthesis()
+        {
+            
+        }
+    }
+}
