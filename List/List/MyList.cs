27bd5514c2bfbfb544c551d1eb4eefc797daa74a
Krosver <kirill.kutyrev@gmail.com>
2022-03-22 10:24:57 +0300

    create MyList

---
+++ List/List/MyList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List
+{
+    class MyList<T>
+    {
+        Node<T> first;
+        Node<T> last;
+        int count;
+
+        void Append(T val)
+        { 
+
+        }
+        void Prepend(T val)
+        {
+            
+        }
+        void Insert(T val)
+        {
+            
+        }
+        T Find(T val)
+        { 
+        }
+        T FindByIndex(int index)
+        {
+        }
+        bool Remove(T val)
+        { 
+        }
+        bool RemoveByIndex(int index)
+        { 
+        }
+        T[] ToArray()
+        { 
+        }
+    }
+}
---
+++ List/List/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List
+{
+    class Node<T>
+    {
+        T val;
+        Node<T> prev;
+        Node<T> next;
+    }
+}
